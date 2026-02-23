4f831d5f3f6381571539b3c9b9763037dba1cab5
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2026-02-23 15:58:11 +0100

    filter dashboard admin done

--- djangotutest/polls/admin.py
+++ djangotutest/polls/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Question,Choice
 
-# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question_text", "pub_date")
+    list_filter = ("pub_date",)
+    ordering = ("-pub_date",)
+    search_fields = ("question_text",)
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
