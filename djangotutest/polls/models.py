d6b04ac134238e17f9094d4a3846301a53158d48
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2026-02-24 11:43:26 +0100

    affichage de la date en plus dans la methode str fait

--- djangotutest/polls/models.py
+++ djangotutest/polls/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
 
     def __str__(self):
-        return self.question_text
+        return f"{self.question_text} ({self.pub_date})"
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
