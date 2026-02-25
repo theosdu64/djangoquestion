678dd1b930fc59aafe4d89c4dcec3c28c7784d51
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2026-02-25 10:24:03 +0100

    partie 3 exercice en cours : ajout dune page all qui repertorie tte les question pour travailler la creation de nouvelle viewauto django + ajout de la page all/frequency qui permet de recuperer le resultat dun en utilisant la methode get_choice du model question

--- djangotutest/polls/urls.py
+++ djangotutest/polls/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("all/", views.AllView.as_view(), name="all"),
+    path("<int:pk>/frequency/", views.FrequencyView.as_view(), name="frequency"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
